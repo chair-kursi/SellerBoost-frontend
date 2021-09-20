d4054b350a2817959ffec5665e48f0cb3d646322
Ayan Khan <ayankhan8223@gmail.com>
2021-09-20 05:06:41 -0700

    table header csv upload complete

--- src/components/services/Dashboard.js
+++ src/components/services/Dashboard.js
@@ -15,19 +15,39 @@ import filterFactory, {
 } from "react-bootstrap-table2-filter";
 import "react-bootstrap-table2-filter/dist/react-bootstrap-table2-filter.min.css";
 import ToolkitProvider, { CSVExport } from "react-bootstrap-table2-toolkit";
+import axios from "axios";
 
 function Dashboard() {
   const [userList, setUserList] = useState([]);
+
   const { ExportCSVButton } = CSVExport;
   const MyExportCSV = (props) => {
     const handleClick = () => {
       props.onExport();
     };
     return (
-      <div>
-        <button className="btn btn-success" onClick={handleClick}>
-          Export to CSV
-        </button>
+      <div className="tableheader">
+        <div className="csv">
+          <div className="csvinputs">
+            <input
+              className="csvinput"
+              type="file"
+              name="csvFile"
+              onChange={onFileChange}
+            />
+          </div>
+          <div className="csvbutton">
+            <button className="butt" onClick={SubmitData}>
+              Submit
+            </button>
+          </div>
+        </div>
+
+        <div className="export">
+          <button className="butt" onClick={handleClick}>
+            Export to CSV
+          </button>
+        </div>
       </div>
     );
   };
@@ -35,31 +55,31 @@ function Dashboard() {
   const columns = [
     // { dataField: "id", text: "Id" },
     {
-      dataField: "styleCode",
+      dataField: "StyleCode",
       text: "StyleCode",
       sort: true,
       filter: textFilter(),
     },
     {
-      dataField: "trafficActual",
+      dataField: "TrafficActual",
       text: "Traffic Actual",
       sort: true,
       filter: textFilter(),
     },
     {
-      dataField: "currentInv",
+      dataField: "CurrentInv",
       text: "Current INV",
       sort: true,
       filter: textFilter(),
     },
     {
-      dataField: "salesNumber",
+      dataField: "SalesNumber",
       text: "Sales Number",
       sort: true,
       filter: textFilter(),
     },
     {
-      dataField: "salesRank",
+      dataField: "SalesRank",
       text: "Sales Rank",
       sort: true,
       filter: textFilter(),
@@ -84,24 +104,49 @@ function Dashboard() {
     },
   });
 
-  useEffect(() => {
-    fetch("http://localhost:3002/api/styletraffic")
-      .then((response) => response.json())
-      .then((result) => setUserList(result))
+  // useEffect(() => {
+  //   fetch("http://localhost:3002/api/styletraffic")
+  //     .then((response) => response.json())
+  //     .then((result) => setUserList(result))
 
-      .catch((error) => console.log(error));
+  //     .catch((error) => console.log(error));
 
-    //   fetch("https://jsonplaceholder.typicode.com/users")
-    //     .then((response) => response.json())
-    //     .then((result) => setUserList(result))
+  //   //   fetch("https://jsonplaceholder.typicode.com/users")
+  //   //     .then((response) => response.json())
+  //   //     .then((result) => setUserList(result))
 
-    //     .catch((error) => console.log(error));
-  }, []);
+  //   //     .catch((error) => console.log(error));
+  // }, []);
   console.log(userList);
+
+  let formData = new FormData();
+
+  const onFileChange = (e) => {
+    console.log(e.target.files[0]);
+    if (e.target && e.target.files[0]) {
+      formData.append("csvFile", e.target.files[0]);
+      console.log(formData);
+    }
+  };
+  const SubmitData = () => {
+    axios
+      .post("http://localhost:3002/api/skuInventory", formData)
+      .then((res) => {
+        console.log(res);
+        setUserList(res.data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
 
   return (
     <div className="Dashboard__table">
       <Header />
+
+      {/* <input type="file" name="csvFile" onChange={onFileChange} />
+      <button onClick={SubmitData}>Submit</button> */}
+
       <ToolkitProvider
         bootstrap4
         keyField="styleCode"
